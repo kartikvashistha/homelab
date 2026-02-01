70f89c1ce347691f09b2ae2fc807879c73b558fa
Kartik Vashistha <kartikv.work@gmail.com>
2026-02-01 20:02:10 +0000

    feat: bunch of changes
    install traefik, refactor var and use helmv3 chart release object

--- k8s/main.go
+++ k8s/main.go
@@ -3,7 +3,8 @@ package main
 import (
 	// appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/apps/v1"
 	// corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/core/v1"
-	helmv4 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/helm/v4"
+	helmv3 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/helm/v3"
+	// helmv4 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/helm/v4"
 	// metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/meta/v1"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
@@ -15,11 +16,11 @@ type HelmChart struct {
 	Repo        string
 	Version     string
 	ValuesFile  pulumi.AssetOrArchiveArray
+	Values      pulumi.Map
 }
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		var ChartList []HelmChart
 
 		cilium := HelmChart{
 			ReleaseName: "cilium",
@@ -27,19 +28,36 @@ func main() {
 			Repo:        "https://helm.cilium.io",
 			Version:     "1.18.5",
 			Namespace:   "kube-system",
+			ValuesFile: pulumi.AssetOrArchiveArray{
+				pulumi.NewFileAsset("./helm-values/cilium.yaml"),
+			},
 		}
 
-		ChartList = append(ChartList, cilium)
+		traefik := HelmChart{
+			ReleaseName: "traefik",
+			Chart:       "traefik",
+			Repo:        "https://traefik.github.io/charts",
+			Version:     "38.0.1",
+			Namespace:   "traefik",
+			ValuesFile: pulumi.AssetOrArchiveArray{
+				pulumi.NewFileAsset("./helm-values/traefik.yaml"),
+			},
+		}
 
-		for _, v := range ChartList {
-			_, err := helmv4.NewChart(ctx, v.ReleaseName, &helmv4.ChartArgs{
+		var HelmReleaseChartList []HelmChart
+		HelmReleaseChartList = append(HelmReleaseChartList, cilium, traefik)
+
+		for _, v := range HelmReleaseChartList {
+			_, err := helmv3.NewRelease(ctx, v.ReleaseName, &helmv3.ReleaseArgs{
 				Chart: pulumi.String(v.Chart),
-				RepositoryOpts: &helmv4.RepositoryOptsArgs{
+				RepositoryOpts: &helmv3.RepositoryOptsArgs{
 					Repo: pulumi.String(v.Repo),
 				},
-				Version:   pulumi.String(v.Version),
-				Name:      pulumi.String(v.ReleaseName),
-				Namespace: pulumi.String(v.Namespace),
+				Version:         pulumi.String(v.Version),
+				Name:            pulumi.String(v.ReleaseName),
+				Namespace:       pulumi.String(v.Namespace),
+				CreateNamespace: pulumi.Bool(true),
+				ValueYamlFiles:  v.ValuesFile,
 			})
 
 			if err != nil {
