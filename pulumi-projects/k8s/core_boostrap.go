e0dcbe306081f66b295cafcccba6c6c52f4a715a
Kartik Vashistha <kartikv.work@gmail.com>
2026-03-28 08:57:04 +0000

    feat: change how stuff works

---
+++ pulumi-projects/k8s/core_boostrap.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	kustomizev2 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/kustomize/v2"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"k8s/internal/metallb"
+)
+
+const (
+	GATEWAYAPI_CRDS_VERSION = "v1.4.0"
+)
+
+func BootstrapCoreServices(ctx *pulumi.Context, k k8sCore) error {
+	if k.Metallb.Install {
+		err := metallb.BootstrapMetallb(ctx, k.Metallb)
+		if err != nil {
+			fmt.Printf("Error during bootstrapping of Metallb: %v", err)
+			return err
+		}
+	}
+
+	if k.InstallGatewayApiCrds {
+		_, err := kustomizev2.NewDirectory(ctx, "gatewayapicrds", &kustomizev2.DirectoryArgs{
+			Directory: pulumi.String(fmt.Sprintf("github.com/kubernetes-sigs/gateway-api/config/crd?ref=%s", GATEWAYAPI_CRDS_VERSION))})
+
+		if err != nil {
+			fmt.Println("Error during the installation of Gateway API CRDs!")
+			return err
+		}
+	}
+	return nil
+}
---
+++ pulumi-projects/k8s/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"k8s/internal/helm"
+	"k8s/internal/metallb"
+	"os"
+
+	"github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/yaml"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
+)
+
+type k8sCore struct {
+	InstallGatewayApiCrds bool
+	Metallb               metallb.Metallb
+}
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		cfg := config.New(ctx, "")
+		kubectx := config.New(ctx, "kubernetes").Require("context")
+
+		var core k8sCore
+		cfg.RequireObject("core", &core)
+		err := BootstrapCoreServices(ctx, core)
+		if err != nil {
+			fmt.Printf("Error during the installation of Core packages!")
+			return err
+		}
+
+		var helmChartList []helm.HelmChart
+		cfg.RequireObject("helm", &helmChartList)
+		for _, v := range helmChartList {
+			file := fmt.Sprintf("./helm-overrides/%s/%s/values.yaml", kubectx, v.ReleaseName)
+			_, err := os.Stat(file)
+
+			if err == nil {
+				v.ValuesFile = file
+				fmt.Println("Value file detected for release: ", file)
+			} else {
+				fmt.Println("Not detected for: ", file)
+			}
+			_, err = helm.CreateHelmRelease(ctx, v)
+			if err != nil {
+				fmt.Println("Error during the creation of helm release!")
+				return err
+			}
+		}
+
+		_, err = yaml.NewConfigGroup(ctx, "manifests",
+			&yaml.ConfigGroupArgs{
+				Files: []string{fmt.Sprintf("./manifests/%s/*.yaml", kubectx)},
+			},
+		)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
