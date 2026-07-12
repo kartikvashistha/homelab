d7cd6e018aaca6b73005d1f647dcc02e7a452c02
Kartik Vashistha <kartikv.work@gmail.com>
2026-07-12 23:25:21 +0100

    feat: last bit of refactor

--- pulumi/projects/k8s/main.go
+++ pulumi/projects/k8s/main.go
@@ -1,77 +1,77 @@
 package main
 
 import (
-	// "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/yaml"
+	"fmt"
+	"os"
+
 	"github.com/kartikvashistha/homelab/pulumi/components/k8s"
+	helmv3 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/helm/v3"
+	"github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/yaml"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
 
+type HelmConfig struct {
+	ReleaseName string
+	Chart       string
+	Namespace   string
+	Repo        string
+	Version     string
+}
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		cfg := config.New(ctx, "")
+		kubectx := config.New(ctx, "kubernetes").Require("context")
 
-		// kubectx := config.New(ctx, "kubernetes").Require("context")
-
-		var n k8s.NetworkComponentArgs
-		cfg.RequireObject("networking", &n)
-		_, err := k8s.SetupNetworkingComponents(ctx, "core-networking", &n)
-
+		var nc k8s.NetworkComponentArgs
+		cfg.RequireObject("networking", &nc)
+		networkComponent, err := k8s.SetupNetworkingComponents(ctx, "core-networking", &nc)
 		if err != nil {
 			return err
 		}
 
-		_, err = k8s.SetupCertManagerComponents(ctx, "cert-manager", &k8s.CertManagerArgs{EnableGatewayAPI: true, InstallCrds: true})
+		certManager, err := k8s.SetupCertManagerComponents(ctx, "cert-manager", &k8s.CertManagerArgs{
+			EnableGatewayAPI: nc.InstallGatewayApiCrds,
+			InstallCrds:      true,
+		})
 		if err != nil {
 			return err
 		}
 
-		// var core k8sCore
-		// cfg.RequireObject("core", &core)
-		// err := bootstrapCoreServices(ctx, core)
-		// if err != nil {
-		// 	fmt.Printf("Error during the installation of Core packages!")
-		// 	return err
-		// }
-		//
-		// err = bootstrapCertManager(ctx, kubectx)
-		// if err != nil {
-		// 	fmt.Println("Error in core service setup: cert-manager")
-		// 	return err
-		// }
-		//
-		// var helmChartList []helm.HelmChart
-		// cfg.RequireObject("helm", &helmChartList)
-		// for _, v := range helmChartList {
-		// 	file := fmt.Sprintf("./helm-overrides/%s/%s/values.yaml", kubectx, v.ReleaseName)
-		// 	_, err := os.Stat(file)
-		//
-		// 	if err == nil {
-		// 		v.ValuesFile = file
-		// 	}
-		//
-		// 	_, err = helm.CreateHelmRelease(ctx, v)
-		// 	if err != nil {
-		// 		fmt.Println("Error during the creation of helm release!")
-		// 		return err
-		// 	}
-		// }
-		//
-		// err = bootstrapDnsResolver(ctx, kubectx)
-		// if err != nil {
-		// 	fmt.Println("Error during installation of coredns-external resolver!")
-		// 	return err
-		// }
-		//
-		// _, err = yaml.NewConfigGroup(ctx, "manifests",
-		// 	&yaml.ConfigGroupArgs{
-		// 		Files: []string{fmt.Sprintf("./manifests/%s/*.yaml", kubectx), fmt.Sprintf("./manifests/%s/*.yml", kubectx)},
-		// 	},
-		// )
-		// if err != nil {
-		// 	return err
-		// }
+		var h []HelmConfig
+		cfg.RequireObject("helm", &h)
+		for _, v := range h {
+			var file string
+			file = fmt.Sprintf("./clusters/%s/helm-overrides/%s/values.yaml", kubectx, v.ReleaseName)
+			_, err := os.Stat(file)
 
+			_, err = helmv3.NewRelease(ctx, v.ReleaseName, &helmv3.ReleaseArgs{
+				Chart:     pulumi.String(v.Chart),
+				Namespace: pulumi.String(v.Namespace),
+				RepositoryOpts: &helmv3.RepositoryOptsArgs{
+					Repo: pulumi.String(v.Repo),
+				},
+				Version:         pulumi.String(v.Version),
+				CreateNamespace: pulumi.Bool(true),
+				ValueYamlFiles:  pulumi.AssetOrArchiveArray{pulumi.NewFileAsset(file)},
+			}, pulumi.DependsOn([]pulumi.Resource{networkComponent, certManager}))
+			if err != nil {
+				return fmt.Errorf("error during the creation of helm release: %v", v.ReleaseName)
+			}
+		}
+
+		_, err = yaml.NewConfigGroup(ctx, "manifests",
+			&yaml.ConfigGroupArgs{
+				Files: []string{
+					fmt.Sprintf("./clusters/%s/manifests/*.yaml", kubectx),
+					fmt.Sprintf("./clusters/%s/manifests/*.yml", kubectx),
+				},
+			}, pulumi.DependsOn([]pulumi.Resource{networkComponent, certManager}),
+		)
+		if err != nil {
+			return fmt.Errorf("error during the creation of yaml config group: %v", err)
+		}
 		return nil
 	})
 }
